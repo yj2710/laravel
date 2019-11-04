24879c77b93397ab9b6604fb48d18d8df0ade560
Yj <18911772710@163.com>
2019-11-04 11:35:54 +0800

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,51 @@ namespace App\Models;
 
 use App\Supports\Model\CamelCase;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model
+class User extends Model implements JWTSubject
 {
     use CamelCase;
+    use Notifiable;
 
-    protected $table = 'user';
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+//    protected $hidden = [
+//        'password', 'email',
+//    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
